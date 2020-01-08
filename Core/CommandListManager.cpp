f99418ba5e54d57e65627d5b1b362c3241be99c4
LisWind <kobbey@163.com>
2020-01-08 22:29:11 +0800

    添加CommandListManager

---
+++ Core/CommandListManager.cpp
@@ -0,0 +1,198 @@
+#include "CommandListManager.h"
+
+using namespace MyDirectX;
+
+CommandQueue::CommandQueue(D3D12_COMMAND_LIST_TYPE type)
+	: m_Type(type), 
+	m_CommandQueue(nullptr), 
+	m_pFence(nullptr), 
+	m_NextFenceValue((uint64_t)type << 56 | 1),			// 高8位存储type信息
+	m_LastCompletedFenceValue((uint64_t)type << 56),
+	m_AllocatorPool(type)
+{
+}
+
+CommandQueue::~CommandQueue()
+{
+	ShutDown();
+}
+
+void CommandQueue::Create(ID3D12Device* pDevice)
+{
+	ASSERT(pDevice != nullptr);
+	ASSERT(!IsReady());
+	ASSERT(m_AllocatorPool.Size() == 0);
+
+	D3D12_COMMAND_QUEUE_DESC cmdQueueDesc = {};
+	cmdQueueDesc.Type = m_Type;
+	cmdQueueDesc.NodeMask = 1;
+	pDevice->CreateCommandQueue(&cmdQueueDesc, IID_PPV_ARGS(&m_CommandQueue));
+	m_CommandQueue->SetName(L"CommandListManager::m_CommandQueue");
+	
+	ASSERT_SUCCEEDED(pDevice->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&m_pFence)));
+	m_pFence->SetName(L"CommandListManager::m_pFence");
+	m_pFence->Signal((uint64_t)m_Type << 56);
+
+	m_FenceEventHandle = CreateEvent(nullptr, false, false, nullptr);
+	ASSERT(m_FenceEventHandle != nullptr);
+
+	m_AllocatorPool.Create(pDevice);
+
+	ASSERT(IsReady());
+
+}
+
+void CommandQueue::ShutDown()
+{
+	if (m_CommandQueue == nullptr)
+		return;
+
+	m_AllocatorPool.Shutdown();
+
+	CloseHandle(m_FenceEventHandle);
+
+	m_pFence->Release();
+	m_pFence = nullptr;
+
+	m_CommandQueue->Release();
+	m_CommandQueue = nullptr;
+
+}
+
+uint64_t CommandQueue::IncrementFence()
+{
+	std::lock_guard<std::mutex> lockGuard(m_FenceMutex);
+
+	m_CommandQueue->Signal(m_pFence, m_NextFenceValue);
+	return m_NextFenceValue++;
+}
+
+bool CommandQueue::IsFenceComplete(uint64_t fenceValue)
+{
+	// avoid querying the fence value by testing against the last one seen.
+	// the max() is to protect against an unlikely race condition that could cause the last
+	// completed fence value to regress（回归，退步）
+	if (fenceValue > m_LastCompletedFenceValue)
+		m_LastCompletedFenceValue = std::max(m_LastCompletedFenceValue, m_pFence->GetCompletedValue());
+
+	return fenceValue <= m_LastCompletedFenceValue;
+}
+
+void CommandQueue::StallForFence(CommandListManager* pCmdListManager, uint64_t fenceValue)
+{
+	CommandQueue& producer = pCmdListManager->GetQueue((D3D12_COMMAND_LIST_TYPE)(fenceValue >> 56));
+	m_CommandQueue->Wait(producer.m_pFence, fenceValue);
+}
+
+void CommandQueue::StallForProducer(CommandQueue& producer)
+{
+	ASSERT(producer.m_NextFenceValue > 0);
+	m_CommandQueue->Wait(producer.m_pFence, producer.m_NextFenceValue - 1);
+}
+
+void CommandQueue::WaitForFence(uint64_t fenceValue)
+{
+	if (IsFenceComplete(fenceValue))
+		return;
+
+	// TODO:  Think about how this might affect a multi-threaded situation.  Suppose thread A
+	// wants to wait for fence 100, then thread B comes along and wants to wait for 99.  If
+	// the fence can only have one event set on completion, then thread B has to wait for 
+	// 100 before it knows 99 is ready.  Maybe insert sequential events?
+	{
+		std::lock_guard<std::mutex> lockGuard(m_EventMutex);
+
+		m_pFence->SetEventOnCompletion(fenceValue, m_FenceEventHandle);
+		WaitForSingleObject(m_FenceEventHandle, INFINITE);
+		m_LastCompletedFenceValue = fenceValue;
+	}
+}
+
+uint64_t CommandQueue::ExecuteCommandList(ID3D12CommandList* list)
+{
+	std::lock_guard<std::mutex> lockGuard(m_FenceMutex);
+
+	ASSERT_SUCCEEDED(((ID3D12GraphicsCommandList*)list)->Close());
+
+	// kickoff the command list
+	m_CommandQueue->ExecuteCommandLists(1, &list);
+
+	// signal the next fence value (with the GPU)
+	m_CommandQueue->Signal(m_pFence, m_NextFenceValue);
+
+	// and increment the fence value
+	return m_NextFenceValue++;
+}
+
+ID3D12CommandAllocator* CommandQueue::RequestAllocator()
+{
+	uint64_t completedFence = m_pFence->GetCompletedValue();
+
+	return m_AllocatorPool.RequestAllocator(completedFence);
+}
+
+void CommandQueue::DiscardAllocator(uint64_t fenceValueForReset, ID3D12CommandAllocator* allocator)
+{
+	m_AllocatorPool.DiscardAllocator(fenceValueForReset, allocator);
+}
+
+// CommandListManager
+CommandListManager::CommandListManager()
+	: m_Device(nullptr),
+	m_GraphicsQueue(D3D12_COMMAND_LIST_TYPE_DIRECT),
+	m_ComputeQueue(D3D12_COMMAND_LIST_TYPE_COMPUTE),
+	m_CopyQueue(D3D12_COMMAND_LIST_TYPE_COPY)
+{
+}
+
+CommandListManager::~CommandListManager()
+{
+	Shutdown();
+}
+
+void CommandListManager::Create(ID3D12Device* pDevice)
+{
+	ASSERT(pDevice != nullptr);
+	
+	m_Device = pDevice;
+
+	m_GraphicsQueue.Create(pDevice);
+	m_ComputeQueue.Create(pDevice);
+	m_CopyQueue.Create(pDevice);
+
+}
+
+void CommandListManager::Shutdown()
+{
+	m_GraphicsQueue.ShutDown();
+	m_ComputeQueue.ShutDown();
+	m_CopyQueue.ShutDown();
+}
+
+void CommandListManager::CreateNewCommandList(D3D12_COMMAND_LIST_TYPE type, ID3D12GraphicsCommandList** list, ID3D12CommandAllocator** allocator)
+{
+	ASSERT(type != D3D12_COMMAND_LIST_TYPE_BUNDLE, "Bundles are not yet supported");
+
+	switch (type)
+	{
+	case D3D12_COMMAND_LIST_TYPE_DIRECT:
+		*allocator = m_GraphicsQueue.RequestAllocator();
+		break;
+	case D3D12_COMMAND_LIST_TYPE_BUNDLE:
+		break;
+	case D3D12_COMMAND_LIST_TYPE_COMPUTE:
+		*allocator = m_ComputeQueue.RequestAllocator();
+		break;
+	case D3D12_COMMAND_LIST_TYPE_COPY:
+		*allocator = m_CopyQueue.RequestAllocator();
+		break;
+	}
+
+	ASSERT_SUCCEEDED(m_Device->CreateCommandList(1, type, *allocator, nullptr, IID_PPV_ARGS(list)));
+	(*list)->SetName(L"CommandList");
+}
+
+void CommandListManager::WaitForFence(uint64_t fenceValue)
+{
+	GetQueue((D3D12_COMMAND_LIST_TYPE)(fenceValue >> 56)).WaitForFence(fenceValue);
+}
---
+++ Core/CommandListManager.h
@@ -0,0 +1,170 @@
+#pragma once
+#include "pch.h"
+#include "CommandAllocatorPool.h"
+#include <queue>
+#include <mutex>
+#include <stdint.h>
+
+namespace MyDirectX
+{
+	class CommandListManager;
+
+	class CommandQueue
+	{
+		/**
+			cppreference.com -> namespace
+			在非局部类X中由友元声明所引入的名字，会成为X的最内层外围命名空间的成员，但它们对于通常的名字
+		查找（无限定或有限定）不可见，除非在命名空间作用域提供与之匹配的声明，不论在类之前还是类之后均可。
+		这种名字可通过ADL找到，其中同时考虑命名空间和类。
+			这种友元声明，在确定名字是否与先前声明的名字冲突时，只考虑最内层外围命名空间。
+		*/
+		friend class CommandListManager;
+		friend class CommandContext;
+
+	public:
+		CommandQueue(D3D12_COMMAND_LIST_TYPE type);
+		~CommandQueue();
+
+		void Create(ID3D12Device* pDevice);
+		void ShutDown();
+
+		inline bool IsReady()
+		{
+			return m_CommandQueue != nullptr;
+		}
+
+		uint64_t IncrementFence();
+		bool IsFenceComplete(uint64_t fenceValue);
+		// 尽管声明友元friend class CommandListManager,这里还是需要声明class CommandListManager
+		void StallForFence(CommandListManager* pCmdListManager, uint64_t fenceValue);
+		void StallForProducer(CommandQueue& producer);
+		void WaitForFence(uint64_t fenceValue);
+		void WaitForIdle() { WaitForFence(IncrementFence()); }
+
+		ID3D12CommandQueue *GetCommandQueue() {}
+
+		uint64_t GetNextFenceValue() {}
+
+	private:
+		uint64_t ExecuteCommandList(ID3D12CommandList* list);
+		ID3D12CommandAllocator* RequestAllocator();
+		void DiscardAllocator(uint64_t fenceValueForReset, ID3D12CommandAllocator* allocator);
+
+		ID3D12CommandQueue* m_CommandQueue;
+
+		const D3D12_COMMAND_LIST_TYPE m_Type;
+
+		CommandAllocatorPool m_AllocatorPool;
+
+		std::mutex m_FenceMutex;
+		std::mutex m_EventMutex;
+
+		// lifetime of these objects is managed by the descriptor cache
+		ID3D12Fence* m_pFence;
+		// high 8 bits - CommandListTye, low 56 bits - actual fence value
+		uint64_t m_NextFenceValue;
+		uint64_t m_LastCompletedFenceValue;
+		HANDLE m_FenceEventHandle;
+	};
+
+	/**
+		the CommandListManager manages all the different CommandQueues: GraphicsQueue, ComputeQueue, and
+	CopyQueue.
+	*/
+	class CommandListManager
+	{
+		friend class CommandContext;
+
+	public:
+		CommandListManager();
+		~CommandListManager();
+
+		void Create(ID3D12Device* pDevice);
+		void Shutdown();
+
+		CommandQueue& GetGraphicsQueue() { return m_GraphicsQueue; }
+		CommandQueue& GetComputeQueue() { return m_ComputeQueue; }
+		CommandQueue& GetCopyQueue() { return m_CopyQueue; }
+
+		CommandQueue& GetQueue(D3D12_COMMAND_LIST_TYPE type = D3D12_COMMAND_LIST_TYPE_DIRECT)
+		{
+			switch (type)
+			{
+			case D3D12_COMMAND_LIST_TYPE_COMPUTE: return m_ComputeQueue;
+			case D3D12_COMMAND_LIST_TYPE_COPY: return m_CopyQueue;
+			default: return m_GraphicsQueue;
+			}
+		}
+
+		ID3D12CommandQueue* GetCommandQueue()
+		{
+			return m_GraphicsQueue.GetCommandQueue();
+		}
+
+		void CreateNewCommandList(
+			D3D12_COMMAND_LIST_TYPE type, 
+			ID3D12GraphicsCommandList **list,
+			ID3D12CommandAllocator **allocator);
+
+		// test to see if a fence has already been reached
+		bool IsFenceComplete(uint64_t fenceValue)
+		{
+			return GetQueue(D3D12_COMMAND_LIST_TYPE(fenceValue >> 56)).IsFenceComplete(fenceValue);
+		}
+
+		// the cpu will wait for a fence to reach a specified value
+		void WaitForFence(uint64_t fenceValue);
+
+		// the cpu will wait for all command queues to empty (so that the cpu is idle)
+		void IdleGPU()
+		{
+			m_GraphicsQueue.WaitForIdle();
+			m_ComputeQueue.WaitForIdle();
+			m_CopyQueue.WaitForIdle();
+		}
+
+	private:
+		ID3D12Device* m_Device;
+
+		CommandQueue m_GraphicsQueue;
+		CommandQueue m_ComputeQueue;
+		CommandQueue m_CopyQueue;
+
+	};
+}
+
+// test
+namespace Test
+{
+	void h(int);
+	namespace A
+	{
+		class X
+		{
+			friend void f(X);	// A::f 友元
+			friend class Z;
+			class Y
+			{
+				friend void g();	// A::g 友元
+				friend void h(int);	// A::h 友元，与Test::h不冲突
+			};
+
+			Z* pz;	// Error: 未定义标识符
+		};
+
+		// A::f, A::g与A::h，Z 在命名空间作用域不可见
+		// 虽然它们是命名空间A的成员
+		// 需要加上声明 
+		class Z;	// 不加出错
+		Z* pz;	// Error: 未定义标识符
+
+		X x;
+		void g() {
+			f(x);	// A::x::f 通过ADL找到
+		}
+		void f(X) { }	// A::f 定义
+		void h(int) { }	// A::h 定义
+		// A::f, A::g, A::h现在在命名空间作用域可见
+		// 而且它们也是A::X A::X::Y 的友元
+	}
+}
